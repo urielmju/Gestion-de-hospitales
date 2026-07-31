13e7d65dd6042f2e875d0cfe06416e517ca77b67
urielmorganbrown@gmail.com <urielmorganbrown@gmail.com>
2026-07-30 19:00:07 -0600

    Registrar excepciones no manejadas en App_Data/error_log.txt para diagnostico

--- Sistemahospital/Global.asax.cs
+++ Sistemahospital/Global.asax.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,27 @@ namespace Sistemahospital
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+            if (ex == null)
+                return;
+
+            try
+            {
+                var ruta = Server.MapPath("~/App_Data/error_log.txt");
+                var texto = string.Format(
+                    "{0:yyyy-MM-dd HH:mm:ss} UTC{1}URL: {2}{1}{3}{1}{4}{1}",
+                    DateTime.UtcNow, Environment.NewLine,
+                    Request?.Url, ex.ToString(),
+                    new string('-', 60));
+                File.AppendAllText(ruta, texto + Environment.NewLine);
+            }
+            catch
+            {
+            }
         }
     }
 }
